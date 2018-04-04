091fa62b1a6d90277d04e653313acd7037e4f65f
NagarajMadyastha <nagaraj.madyastha.cr@gmail.com>
2018-04-04 20:44:21 +0530

    removed css entry from ts file

--- src/app/module/first/first.component.ts
+++ src/app/module/first/first.component.ts
@@ -2,9 +2,8 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'first',
-  templateUrl: './first.component.html',
-  styleUrls: ['./first.component.css']
-})
+  templateUrl: './first.component.html'
+  })
 export class FirstComponent implements OnInit {
 
   constructor() { }
